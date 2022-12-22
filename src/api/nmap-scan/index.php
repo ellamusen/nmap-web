ed703970fc7be530421a134961d610d05444907b
ellamusen <dani_1997@live.dk>
2022-12-22 17:56:21 +0100

    Added host filter

--- src/api/nmap-scan/index.php
+++ src/api/nmap-scan/index.php
@@ -4,6 +4,10 @@
 $data = json_decode(file_get_contents('php://input'), true);
 $host = $data["host"];
 
+if (!str_starts_with($host, "127.0.0.1") && !str_starts_with($host, "localhost")) {
+    echo "You can only scan 127.0.0.1 or localhost!";
+    return null;
+}
 system("nmap $host");
 
 ?>
